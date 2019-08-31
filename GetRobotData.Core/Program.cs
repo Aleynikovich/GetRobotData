a9c3c2dd5c9343e19bb2d908c4d54dbad55efa46
Alexander Kalis <alexander.kalis@kukarob.es>
2019-08-31 03:44:43 +0200

    Hello world.

---
+++ GetRobotData.Core/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace GetRobotData.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ StringManipulation/StringManipulation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringManipulation
+{
+
+    public class StringManipulation
+    { 
+        public static string GetBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0, StringComparison.Ordinal) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start, StringComparison.Ordinal);
+
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "Limit strings not found";
+            }
+        }
+    }
+   
+}
